b9e26ea5d4b4db662194cef8c3155d81b82a2054
Std <student@Lab-1302.(none)>
2013-02-05 16:00:39 +0700

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
 	def index
+ 	 @user = User.all
+  		respond_to do |format|
+   		format.html  # index.html.erb
+    	format.json  { render :json => @user }
+	end
 	end
 
 	def new
@@ -21,9 +26,23 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+ 		 @user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+ 
+  		respond_to do |format|
+    	if @user.update_attributes(params[:user])
+      format.html  { redirect_to(@user,
+                    :notice => 'User was successfully updated.') }
+      format.json  { head :no_content }
+    else
+      format.html  { render :action => "edit" }
+      format.json  { render :json => @user.errors,
+                    :status => :unprocessable_entity }
+    end
+  end
 	end
 
 	def destroy
